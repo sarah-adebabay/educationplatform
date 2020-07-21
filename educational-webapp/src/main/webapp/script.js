f4debebea803c188614e7947544931a253d3f2d8
Sarah <sarah_adebabay@berkeley.edu>
2020-07-20 23:25:57 -0700

    Added the onclick alphabetAudio

--- educational-webapp/src/main/webapp/script.js
+++ educational-webapp/src/main/webapp/script.js
@@ -1,33 +1,81 @@
-var aAudio = new Audio('sounds/a.m4a');
-var bAudio = new Audio('sounds/b.m4a');
-var cAudio = new Audio('sounds/c.m4a');
-var dAudio = new Audio('sounds/d.m4a');
-var eAudio = new Audio('sounds/e.m4a');
-var fAudio = new Audio('sounds/f.m4a');
-var gAudio = new Audio('sounds/g.m4a');
-var hAudio = new Audio('sounds/h.m4a');
-var iAudio = new Audio('sounds/i.m4a');
-var jAudio = new Audio('sounds/j.m4a');
-var kAudio = new Audio('sounds/k.m4a');
-var lAudio = new Audio('sounds/l.m4a');
-var mAudio = new Audio('sounds/m.m4a');
-var nAudio = new Audio('sounds/n.m4a');
-var oAudio = new Audio('sounds/o.m4a');
-var pAudio = new Audio('sounds/p.m4a');
-var qAudio = new Audio('sounds/q.m4a');
-var rAudio = new Audio('sounds/r.m4a');
-var sAudio = new Audio('sounds/s.m4a');
-var tAudio = new Audio('sounds/t.m4a');
-var uAudio = new Audio('sounds/u.m4a');
-var vAudio = new Audio('sounds/v.m4a');
-var xAudio = new Audio('sounds/x.m4a');
-var yAudio = new Audio('sounds/y.m4a');
-var zAudio = new Audio('sounds/z.m4a');
+var aAudio = new Audio('sounds/A.wav');
+var bAudio = new Audio('sounds/B.m4a');
+var cAudio = new Audio('sounds/C.m4a');
+var dAudio = new Audio('sounds/D.m4a');
+var eAudio = new Audio('sounds/E.m4a');
+var fAudio = new Audio('sounds/F.m4a');
+var gAudio = new Audio('sounds/G.m4a');
+var hAudio = new Audio('sounds/H.m4a');
+var iAudio = new Audio('sounds/I.m4a');
+var jAudio = new Audio('sounds/J.m4a');
+var kAudio = new Audio('sounds/K.m4a');
+var lAudio = new Audio('sounds/L.m4a');
+var mAudio = new Audio('sounds/M.m4a');
+var nAudio = new Audio('sounds/N.m4a');
+var oAudio = new Audio('sounds/O.m4a');
+var pAudio = new Audio('sounds/P.m4a');
+var qAudio = new Audio('sounds/Q.m4a');
+var rAudio = new Audio('sounds/R.m4a');
+var sAudio = new Audio('sounds/S.m4a');
+var tAudio = new Audio('sounds/T.m4a');
+var uAudio = new Audio('sounds/U.m4a');
+var vAudio = new Audio('sounds/V.m4a');
+var xAudio = new Audio('sounds/X.m4a');
+var yAudio = new Audio('sounds/Y.m4a');
+var zAudio = new Audio('sounds/Z.m4a');
 
 function alphabetAudio(letter) {
     if(letter == 'a') {
-        testAudio.play();
+        aAudio.play();
     } else if(letter == 'b') {
         bAudio.play();
+    } else if(letter == 'c') {
+        cAudio.play();
+    } else if(letter == 'd') {
+        dAudio.play();
+    } else if(letter == 'e') {
+        eAudio.play();
+    } else if(letter == 'f') {
+        fAudio.play();
+    } else if(letter == 'g') {
+        gAudio.play();
+    } else if(letter == 'h') {
+        hAudio.play();
+    } else if(letter == 'i') {
+        iAudio.play();
+    } else if(letter == 'j') {
+        jAudio.play();
+    } else if(letter == 'k') {
+        kAudio.play();
+    } else if(letter == 'l') {
+        lAudio.play();
+    } else if(letter == 'm') {
+        mAudio.play();
+    } else if(letter == 'n') {
+        nAudio.play();
+    } else if(letter == 'o') {
+        oAudio.play();
+    } else if(letter == 'p') {
+        pAudio.play();
+    } else if(letter == 'q') {
+        qAudio.play();
+    } else if(letter == 'r') {
+        rAudio.play();
+    } else if(letter == 's') {
+        sAudio.play();
+    } else if(letter == 't') {
+        tAudio.play();
+    } else if(letter == 'u') {
+        uAudio.play();
+    } else if(letter == 'v') {
+        vAudio.play();
+    } else if(letter == 'w') {
+        wAudio.play();
+    } else if(letter == 'x') {
+        xAudio.play();
+    } else if(letter == 'y') {
+        yAudio.play();
+    } else if(letter == 'z') {
+        zAudio.play();
     }
 }

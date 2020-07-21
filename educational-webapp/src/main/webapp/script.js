4d8a383e84b34b5ce9f2e285035e11a5df7b4a52
Sarah <sarah_adebabay@berkeley.edu>
2020-07-20 22:57:10 -0700

    adding sounds

--- educational-webapp/src/main/webapp/script.js
+++ educational-webapp/src/main/webapp/script.js
@@ -1,3 +1,33 @@
-function onClick(){
-    console.log('Letter');
+var aAudio = new Audio('sounds/a.m4a');
+var bAudio = new Audio('sounds/b.m4a');
+var cAudio = new Audio('sounds/c.m4a');
+var dAudio = new Audio('sounds/d.m4a');
+var eAudio = new Audio('sounds/e.m4a');
+var fAudio = new Audio('sounds/f.m4a');
+var gAudio = new Audio('sounds/g.m4a');
+var hAudio = new Audio('sounds/h.m4a');
+var iAudio = new Audio('sounds/i.m4a');
+var jAudio = new Audio('sounds/j.m4a');
+var kAudio = new Audio('sounds/k.m4a');
+var lAudio = new Audio('sounds/l.m4a');
+var mAudio = new Audio('sounds/m.m4a');
+var nAudio = new Audio('sounds/n.m4a');
+var oAudio = new Audio('sounds/o.m4a');
+var pAudio = new Audio('sounds/p.m4a');
+var qAudio = new Audio('sounds/q.m4a');
+var rAudio = new Audio('sounds/r.m4a');
+var sAudio = new Audio('sounds/s.m4a');
+var tAudio = new Audio('sounds/t.m4a');
+var uAudio = new Audio('sounds/u.m4a');
+var vAudio = new Audio('sounds/v.m4a');
+var xAudio = new Audio('sounds/x.m4a');
+var yAudio = new Audio('sounds/y.m4a');
+var zAudio = new Audio('sounds/z.m4a');
+
+function alphabetAudio(letter) {
+    if(letter == 'a') {
+        testAudio.play();
+    } else if(letter == 'b') {
+        bAudio.play();
+    }
 }

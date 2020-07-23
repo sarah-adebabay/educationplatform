6d7b4ed61f7c566665b99221a1feb47d80e27e7c
Sarah Adebabay <51680408+sarah-adebabay@users.noreply.github.com>
2020-07-22 22:33:04 -0700

    Added alphabet sounds while clicking the alphabet icons (#10)
    * adding sounds
    
    * Added the onclick alphabetAudio
    
    * update file names

--- educational-webapp/src/main/webapp/script.js
+++ educational-webapp/src/main/webapp/script.js
@@ -1,3 +1,81 @@
-function onClick(){
-    console.log('Letter');
+var aAudio = new Audio('sounds/A.wav');
+var bAudio = new Audio('sounds/B.wav');
+var cAudio = new Audio('sounds/C.wav');
+var dAudio = new Audio('sounds/D.wav');
+var eAudio = new Audio('sounds/E.wav');
+var fAudio = new Audio('sounds/F.wav');
+var gAudio = new Audio('sounds/G.wav');
+var hAudio = new Audio('sounds/H.wav');
+var iAudio = new Audio('sounds/I.wav');
+var jAudio = new Audio('sounds/J.wav');
+var kAudio = new Audio('sounds/K.wav');
+var lAudio = new Audio('sounds/L.wav');
+var mAudio = new Audio('sounds/M.wav');
+var nAudio = new Audio('sounds/N.wav');
+var oAudio = new Audio('sounds/O.wav');
+var pAudio = new Audio('sounds/P.wav');
+var qAudio = new Audio('sounds/Q.wav');
+var rAudio = new Audio('sounds/R.wav');
+var sAudio = new Audio('sounds/S.wav');
+var tAudio = new Audio('sounds/T.wav');
+var uAudio = new Audio('sounds/U.wav');
+var vAudio = new Audio('sounds/V.wav');
+var xAudio = new Audio('sounds/X.wav');
+var yAudio = new Audio('sounds/Y.wav');
+var zAudio = new Audio('sounds/Z.wav');
+
+function alphabetAudio(letter) {
+    if(letter == 'a') {
+        aAudio.play();
+    } else if(letter == 'b') {
+        bAudio.play();
+    } else if(letter == 'c') {
+        cAudio.play();
+    } else if(letter == 'd') {
+        dAudio.play();
+    } else if(letter == 'e') {
+        eAudio.play();
+    } else if(letter == 'f') {
+        fAudio.play();
+    } else if(letter == 'g') {
+        gAudio.play();
+    } else if(letter == 'h') {
+        hAudio.play();
+    } else if(letter == 'i') {
+        iAudio.play();
+    } else if(letter == 'j') {
+        jAudio.play();
+    } else if(letter == 'k') {
+        kAudio.play();
+    } else if(letter == 'l') {
+        lAudio.play();
+    } else if(letter == 'm') {
+        mAudio.play();
+    } else if(letter == 'n') {
+        nAudio.play();
+    } else if(letter == 'o') {
+        oAudio.play();
+    } else if(letter == 'p') {
+        pAudio.play();
+    } else if(letter == 'q') {
+        qAudio.play();
+    } else if(letter == 'r') {
+        rAudio.play();
+    } else if(letter == 's') {
+        sAudio.play();
+    } else if(letter == 't') {
+        tAudio.play();
+    } else if(letter == 'u') {
+        uAudio.play();
+    } else if(letter == 'v') {
+        vAudio.play();
+    } else if(letter == 'w') {
+        wAudio.play();
+    } else if(letter == 'x') {
+        xAudio.play();
+    } else if(letter == 'y') {
+        yAudio.play();
+    } else if(letter == 'z') {
+        zAudio.play();
+    }
 }
